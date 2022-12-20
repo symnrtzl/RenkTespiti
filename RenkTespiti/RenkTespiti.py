558b936d4f107662586227242c5337577bb7f8c7
ZehraSahaplioglu <99864727+ZehraSahaplioglu@users.noreply.github.com>
2022-12-20 22:43:11 +0300

    Update RenkTespiti.py
    zehra add first commit

--- RenkTespiti/RenkTespiti.py
+++ RenkTespiti/RenkTespiti.py
@@ -13,7 +13,20 @@ hsv_image=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
 low_yellow=np.array([20, 120, 80])
 high_yellow=np.array( [45, 200, 255])
 
+
 mask= cv2.inRange(hsv_image, low_yellow, high_yellow)
+mask= cv2.erode(mask,None,iterations=2)
+mask=cv2.dilate(mask,None,iterations=2)
+mask=cv2.GaussianBlur(mask,(3,3),0)
+cnts=cv2.findContours(mask.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+
+#Zehra add pull request 
+
+low_red=np.array([170,50,70])
+high_red=np.array([180,255,255])
+
+mask= cv2.inRange(hsv_image, low_red, high_red)
 mask= cv2.erode(mask,None,iterations=2)
 mask=cv2.dilate(mask,None,iterations=2)
 mask=cv2.GaussianBlur(mask,(3,3),0)
@@ -27,3 +40,5 @@ for cnt in cnts:
 cv2.imshow("Sonuc",image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
